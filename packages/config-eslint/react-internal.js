3ace61f0e26e820b60d45bd1e9c09f7b38cd00e4
Juan Ignacio De los Santos <juanignaciodelossantos01@gmail.com>
2025-05-11 10:34:55 -0300

    chore: configured other things in the project

---
+++ packages/config-eslint/react-internal.js
@@ -0,0 +1,22 @@
+import { config as baseConfig } from './base.js';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+
+/**
+ * A custom ESLint configuration for libraries that use React.
+ *
+ * @type {import("eslint").Linter.Config[]} */
+export const config = [
+  ...baseConfig,
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      'react-hooks': pluginReactHooks,
+    },
+    settings: { react: { version: 'detect' } },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+    },
+  },
+];
